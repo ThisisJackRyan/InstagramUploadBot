f012ae627edb9d16b59801b943461138a312375c
Jack C. Ryan <jackcryan@code21-312.rockhurst.int>
2022-05-11 10:21:14 -0500

    2nd commit

--- InstagramMain.py
+++ InstagramMain.py
@@ -2,7 +2,8 @@ import os
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-Path = "C:\Program Files (x86)\chromedriver.exe"
+#Path = "C:\Program Files (x86)\chromedriver.exe" #for Windows
+Path = "/Users/jackcryan/Downloads/chromeDrivers/chromedriver"
 driver = webdriver.Chrome(Path)
 
 #driver.get("https://github.com/login")
@@ -17,9 +18,20 @@ driver.get("https://www.instagram.com/")
 time.sleep(5)
 instaEmail = driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input')
 instapassword = driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input')
-instaEmail.send_keys("")
-instapassword.send_keys("")
+instaEmail.send_keys("thisisjackryan@icloud.com")
+instapassword.send_keys("nODDOG1021")
 instapassword.send_keys(Keys.RETURN)
+
+time.sleep(10)
+
+notNow = driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div/div/button')
+notNow.click()
+
+time.sleep(3)
+
+notifictationNotNow = driver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]')
+notifictationNotNow.click()
+#this poped up twice so fix that 
 
 
 
