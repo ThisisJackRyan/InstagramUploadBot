4f9ed47943d17711ab96b4ae5d0e3d6c91d01ba4
ThisisJackRyan <thisisjackryan1@gmail.com>
2023-07-24 12:14:11 -0500

    add basic header outline

--- react-gui/src/App.js
+++ react-gui/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Componets/Header';
 
 function App() {
   return (
     <div className="App">
-      My APP
+      <Header />
     </div>
   );
 }
---
+++ react-gui/src/Componets/Header.jsx
@@ -0,0 +1,17 @@
+
+
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerTitle">Bot Central</div>
+            <div className="nav">
+                <div>Bots</div>
+                <div>Account</div>
+                <div>Setting</div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
