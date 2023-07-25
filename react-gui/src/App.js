1847770f8ed6c749809c3c27564aecb5e6b18d0d
ThisisJackRyan <thisisjackryan1@gmail.com>
2023-07-24 21:34:19 -0500

    basic hompage format

--- react-gui/src/App.js
+++ react-gui/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Componets/Header';
+import MainPage from './Componets/Pages/MainPage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MainPage />
     </div>
   );
 }
---
+++ react-gui/src/Componets/Pages/MainPage.jsx
@@ -0,0 +1,25 @@
+import instaHomePageIMG from "../Assets/Phones.png"
+
+
+const MainPage = () => {
+    return (
+        <div className="flex instagramBackground">
+            <div className="Content">
+                <img src={instaHomePageIMG} alt="" />
+            </div>
+            <div className="QuickDescription">
+                <h1>Simplify Your Posting Process!</h1>
+                <p>
+                Are you tired of the time-consuming process of manually uploading photos and videos to your Instagram account? 
+                Look no further! 
+                Take your Instagram game to the next level with our powerful Instagram Uploader Bot. 
+                </p>
+                <div className="btn">
+                    <button>Run Bot</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MainPage;
